a38962dd719eabbb106417639eb41c0cc16b53f7
krishpe <64515207+krishpe@users.noreply.github.com>
2020-06-17 14:00:55 +0530

    Release V1.3.0 (#12)
    * Release V1.3.0
    
    * Update README.md
    
    * Update package.json

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 export { default as GMapify } from "./Gmapify";
 export { default as AddressFormatter } from "./utils/addressFormatter_";
+
+const pjson = require("../package.json");
+console.log(
+  ` >> 🗺🗺🗺 G-Mapify : Version: ${pjson.version}, Build: ${process.env.NODE_ENV} <<`
+);
