3d74c5b9886bbcbe507928b83a32ce61a30a3277
sandeep-r-bharatpe <sandeep.rawat@bharatpe.com>
2021-02-16 02:16:45 +0530

    fix: g-mapify fail on initialisation twice [#22]

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,11 +1,79 @@
+let isMapLoaded = false;
+let isMapLoadPending = false;
+let scriptElem = null;
+const resolveList = [];
+const rejectList = [];
+
 const getMapScript = (appKey, libraries) =>
   `https://maps.googleapis.com/maps/api/js?key=${appKey}&callback=initMapScript&libraries=${libraries}`;
 
+/**
+ * @function notifyAll
+ * @param {boolean} isResolve
+ * @description notify all component to map script downloaded
+ */
+const notifyAll = (isResolve) => {
+  if (isResolve) {
+    for (let i = 0; i < resolveList.length; i++) {
+      resolveList[i]();
+    }
+  } else {
+    for (let i = 0; i < rejectList.length; i++) {
+      rejectList[i](new Error("map script not loaded"));
+    }
+  }
+  resolveList.length = 0;
+  rejectList.length = 0;
+};
+
+/**
+ * @function injectMapScript
+ * @param {string} appKey
+ * @param {string} libraries
+ * @description download map script file
+ */
 const injectMapScript = (appKey, libraries) => {
-  const scriptElem = document.createElement("script");
-  scriptElem.src = getMapScript(appKey, libraries);
-  scriptElem.setAttribute("id", "google-map");
-  document.querySelector("head").appendChild(scriptElem);
+  if (isMapLoaded) return Promise.resolve();
+
+  return new Promise((resolve, reject) => {
+    if (!window.initMapScript) {
+      window.initMapScript = () => {
+        console.log("Map script successful");
+        notifyAll(true);
+      };
+    }
+
+    resolveList.push(resolve);
+    rejectList.push(reject);
+
+    function mapScriptLoadEvent() {
+      isMapLoaded = true;
+      isMapLoadPending = false;
+      scriptElem.setAttribute("loaded", "true");
+      scriptElem.removeEventListener("load", mapScriptLoadEvent);
+      scriptElem.removeEventListener("error", mapScriptErrorEvent);
+    }
+
+    function mapScriptErrorEvent() {
+      isMapLoaded = false;
+      isMapLoadPending = false;
+      scriptElem.setAttribute("loaded", "false");
+      scriptElem.removeEventListener("error", mapScriptErrorEvent);
+      scriptElem.removeEventListener("load", mapScriptLoadEvent);
+      document.head.removeChild(scriptElem);
+      notifyAll(false);
+    }
+
+    if (!isMapLoadPending) {
+      scriptElem = document.createElement("script");
+      scriptElem.addEventListener("load", mapScriptLoadEvent);
+      scriptElem.addEventListener("error", mapScriptErrorEvent);
+      scriptElem.src = getMapScript(appKey, libraries);
+      scriptElem.setAttribute("id", "google-map");
+      document.querySelector("head").appendChild(scriptElem);
+      isMapLoadPending = true;
+    }
+  });
 };
 
 /**
